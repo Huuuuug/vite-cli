c5af18eb1452deb73fdf7e8f9c953ca3508217cf
胡钢 <18768366022@163.com>
2022-09-28 10:33:19 +0800

    11

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,7 +13,7 @@ const count = ref(0)
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
       Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      <code>components/HelloWorld.vue</code> to test 11HMR
     </p>
   </div>
 
