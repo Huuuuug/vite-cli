97002fca47074c208e32db43c33a9df9f97a644e
胡钢 <18768366022@163.com>
2022-09-29 12:04:44 +0800

    feat: template--init

--- src/view/Index.vue
+++ src/view/Index.vue
@@ -3,15 +3,4 @@
 </template>
 <script lang="ts" setup>
 import { NButton } from 'naive-ui'
-import { ref } from 'vue'
-console.log(1111111111123123)
-console.log(1111111111123123)
-console.log(1111111111123123)
-console.log(1111111111123123)
-
-console.log(1111111111123123)
-
-console.log(1111111111123123)
-
-const count = ref(111)
 </script>
